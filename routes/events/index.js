7a93a43c0fb01ca7b7783adf316edb50467bd12c
Antti Leppä <antti.leppa@metatavu.fi>
2018-05-03 10:27:12 +0300

    Fixed issue with event date ranges

--- routes/events/index.js
+++ routes/events/index.js
@@ -118,7 +118,7 @@
     app.get('/ajax/events', (req, res, next) => {
       const perPage = Common.EVENTS_COUNT_PAGE;
       const page = parseInt(req.query.page)||0;
-      const start = req.query.start ? moment(req.query.start, 'DD.MM.YYYY').startOf('day').format() : null;
+      const start = req.query.start ? moment(req.query.start, 'DD.MM.YYYY') : moment();
       const end = req.query.end ? moment(req.query.end, 'DD.MM.YYYY').endOf('day').format() : null;
       const module = new ModulesClass(config);
         
@@ -127,8 +127,8 @@
         maxResults: perPage + 1,
         orderBy: 'START_DATE',
         orderDir: 'DESCENDING',
-        startBefore: end,
-        startAfter: start
+        endBefore: end,
+        startAfter: start.startOf('day').format()
       })
       .callback((data) => {
         const lastPage = data[0].length < perPage + 1;
