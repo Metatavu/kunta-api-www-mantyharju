2f141b3aedecb00ace8d9485d4248418a5a82304
Antti Leppä <antti.leppa@metatavu.fi>
2019-10-31 08:56:30 +0200

    Hound issues

--- public/js/event-new.js
+++ public/js/event-new.js
@@ -1,28 +1,28 @@
-/* global bootbox */
+/* global bootbox,$ */
 
 (function () {
-  'use strict';
+  "use strict";
   
   $(document).on("click", ".new-place-link", function (event) {
     event.preventDefault();
     
-    $.get('/ajax/linkedevents/places/new', function (response) {
+    $.get("/ajax/linkedevents/places/new", function (response) {
       var dialog = bootbox.dialog({ 
-        title: 'Uusi tapahtumapaikka',
+        title: "Uusi tapahtumapaikka",
         message: response,
         className: "new-place-dialog"
       });
       
-      $('.new-place-dialog .metaform').metaform({
+      $(".new-place-dialog .metaform").metaform({
         onPostSuccess: function (response) {
           var value = response.id;
           var label = response.name.fi || response.name.sv || response.name.en;
           
-          $('#field-location').metaformAutocomplete('val', {
+          $("#field-location").metaformAutocomplete("val", {
             value: value,
             label: label
           });
-           
+
           dialog.modal("hide");
         }
       });
@@ -31,9 +31,9 @@
   });
 
   $(document).ready(function () {
-    $('.metaform').metaform('option', 'onPostSuccess', function (response) {
+    $(".metaform").metaform("option", "onPostSuccess", function (response) {
       bootbox.alert({
-        message: '<i class="fa fa-check" /><h3>Tapahtuma  on lähetetty ylläpidolle arvioitavaksi.</h3>',
+        message: "<i class=\"fa fa-check\" /><h3>Tapahtuma  on lähetetty ylläpidolle arvioitavaksi.</h3>",
         backdrop: true,
         callback: function(){
           window.location.reload(true);
@@ -41,21 +41,21 @@
       });
     });
 
-    $('.form-control').addClass('pristine');
+    $(".form-control").addClass("pristine");
 
-    $('.form-control').focus(function(){
-      $(this).removeClass('pristine');
+    $(".form-control").focus(function(){
+      $(this).removeClass("pristine");
     });
 
-    $('.form-control').change(function(){
-      $(this).removeClass('pristine');
+    $(".form-control").change(function(){
+      $(this).removeClass("pristine");
     });
 
-    $(document).on('click', '.flatpickr-input', function(){
-      $(this).removeClass('pristine');
+    $(document).on("click", ".flatpickr-input", function(){
+      $(this).removeClass("pristine");
     });
 
-    $('.metaform').addClass("ready");
+    $(".metaform").addClass("ready");
   });
   
 }).call(this);
